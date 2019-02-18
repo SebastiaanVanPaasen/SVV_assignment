0ddd42d9061c2c5470841a1dc35d2ae8a493a6a7
sorinsky <47746150+sorinsky@users.noreply.github.com>
2019-02-18 20:39:47 +0100

    Update xyarclen.py

--- xyarclen.py
+++ xyarclen.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 18 15:38:58 2019
+Created on Mon Feb 18 19:18:22 2019
 
-@author: sseremet
+@author: Sorin
 """
 from numpy import *
 import matplotlib.pyplot as plt
-
+circlen=pi*17.3/4
+stotal=pi*17.3/4+sqrt(((48.4-17.3/2))**2+(17.3/2)**2)
 xlist=[]
 ylist=[]
 def xyarclen(r,s,ca):
@@ -21,7 +22,8 @@ def xyarclen(r,s,ca):
 #Only calculates positive y
 #for s in range(0,pi*17.3/4+sqrt((48.4-17.3/2)**2+(17.3/2)**2)):
 for s in range(0,54):
-    a,b=xyarclen(17.3/2,s,0.484)
+    a,b=xyarclen(17.3/2,s,48.4)
     xlist.append(a)
     ylist.append(b)
 plt.plot(xlist,ylist)
+plt.axis('equal')
