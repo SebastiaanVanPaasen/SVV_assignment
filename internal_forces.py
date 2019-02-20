2763d50f2ce89e01f5b9217178a2fef766f8469d
Unknown <sebas.98vp@gmail.com>
2019-02-20 12:18:11 +0100

    Additional equations

--- internal_forces.py
+++ internal_forces.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-# important x-locations along the beam
+# aileron parameters
+l_a = 1.691
+h_a = 0.173
+
+# force locations along the beam
 x_begin = 0
 x_1 = 0.149
 x_2 = 0.554
@@ -10,23 +14,71 @@ x_end = 1.691
 # actuator distances
 x_a = 0.272
 x_a1 = x_2 - x_a/2
+y_a1 = np.cos(np.radians(h_a/2 - np.tan(np.radians(28))*h_a/2))
+z_a1 = np.cos(np.radians(28))*h_a/2 + np.sin(np.radians(28))*h_a/2
 x_a2 = x_2 + x_a/2
 
-# other parameters
-l_a = 1.691
+# load parameters
 q = 2710
 p = 37900
 E = 1
 
-x_coordinates = [x_begin, x_1, x_a1, x_2, x_a2, x_3, x_end]
+
+class Force:
+
+    def __init__(self, magnitude, direction, position):
+        self.magnitude = magnitude
+        self.direction = direction
+        self.position = position
+
+    def determine_force(self, direction):
+        determinator = 0
+        if direction == 'y':
+            determinator = 1
+        elif direction == 'z':
+            determinator = 2
+
+        if self.direction[determinator] != 0:
+            return self.magnitude*self.direction[determinator]
+        else:
+            return 0
+
+    def determine_moment(self, position):
+        distance = self.position - position
+        moments = np.zeros(3)
+
+        moments[0] = self.magnitude*-1*(self.direction[1]*distance[2] + self.direction[2]*distance[1])
+        moments[1] = self.magnitude*-1*(self.direction[0]*distance[2] + self.direction[2]*distance[0])
+        moments[2] = self.magnitude*-1*(self.direction[0]*distance[1] + self.direction[1]*distance[0])
+
+        return moments
 
 
-y_forces = {'y_1': [1, x_1], 'y_2': [1, x_2], 'y_3': [1, x_3]}
-z_forces = {'z_1': [1, x_1], 'a_1': [1, x_a1], 'z_2': [1, x_2], 'a_2': [p, x_a2], 'z_3': [1, x_3]}
+y_1 = Force(1, np.array([0, 1, 0]), np.array([x_1, 0, 0]))
+y_2 = Force(1, np.array([0, 1, 0]), np.array([x_2, 0, 0]))
+y_3 = Force(1, np.array([0, 1, 0]), np.array([x_3, 0, 0]))
+z_1 = Force(1, np.array([0, 0, 1]), np.array([x_1, 0, 0]))
+z_2 = Force(1, np.array([0, 0, 1]), np.array([x_2, 0, 0]))
+z_3 = Force(1, np.array([0, 0, 1]), np.array([x_3, 0, 0]))
+a_1 = Force(1, np.array([0, 0, 1]), np.array([x_a1, y_a1, z_a1]))
+q_ = Force(q*l_a, np.array([0, -1, 0]), np.array([l_a/2, 0, 0]))
+p_ = Force(p, np.array([0, 0, -1]), np.array([x_a2, y_a1, z_a1]))
+
+forces = [y_1, y_2, y_3, z_1, z_2, z_3, a_1, q_, p_]
 
 sum_forces_y = []
-sum_moments_x = []
+sum_forces_z = []
+sum_moments = []
 
-for force in y_forces:
-    sum_forces_y.append(y_forces[force][0])
-    sum_moments_x.append(y_forces[force] * (y_forces[force][1] - x_begin))
+for force in forces:
+    sum_forces_y.append(force.determine_force('y'))
+    sum_forces_z.append(force.determine_force('z'))
+    sum_moments.append(force.determine_moment([0, 0, 0]))
+
+print(sum_forces_y)
+print(sum_forces_z)
+print(sum_moments)
+
+ce_eq = np.array([(l_a - x_1)**3/3, (l_a - x_2)**3/3 - (x_1+x_2)*(l_a - x_2)**2/2 + x_1*x_2*(l_a - x_2), (l_a - x_3)**3/3 - (x_1)])
+
+
