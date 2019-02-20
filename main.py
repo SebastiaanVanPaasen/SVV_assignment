1be7561ad7a6ea3b21e9e680416f3180b374a28c
lwelzel <29613344+lwelzel@users.noreply.github.com>
2019-02-20 21:01:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ def read_table(filename):  # tables must be in form: 1 row header (description),
     file = open(filename, "r+")
     file = file.readlines()
     n_colums = len(file[0].split('\t'))
-    data=np.array(ndmin=2)
     del file[0]
-    for line in file:
-        l = line.split('\t')
-
-
-    for i in (ps1, ps2, t):
-        del i[0]
-    for i in range(len(t)):
-        floatt.append(float(t[i]))
-        floatps1.append(float(ps1[i]))
-        floatps2.append(float(ps2[i]))
-    return (floatt, floatps1, floatps2)
+    n_rows = len(file)
+    data=np.zeros(shape=(n_colums,n_rows))
+    for i in range(n_rows):
+        l = file[i].split('\t')
+        for j in range(n_colums):
+            data[j,i]=l[j]
+    return data
