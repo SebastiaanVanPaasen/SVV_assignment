815c2d7db6443f65d93b085c930a6968f5874b69
lwelzel <29613344+lwelzel@users.noreply.github.com>
2019-02-20 20:39:06 +0100

    Update geometric_properties.py

--- geometric_properties.py
+++ geometric_properties.py
@@ -174,24 +174,4 @@ def main():
     plt.show()
     return
 
-
-def read_table(filename):  # tables must be in form: 1 row header (description), separation by tabulators
-    file = open(filename, "r+")
-    file = file.readlines()
-    n_colums = len(file[0].split('\t'))
-    data=np.array(ndmin=2)
-    del file[0]
-    for line in file:
-        l = line.split('\t')
-
-
-    for i in (ps1, ps2, t):
-        del i[0]
-    for i in range(len(t)):
-        floatt.append(float(t[i]))
-        floatps1.append(float(ps1[i]))
-        floatps2.append(float(ps2[i]))
-    return (floatt, floatps1, floatps2)
-
-
 main()
